a0e02d254e37b4e585aa106ee8a9a231dc6ab421
Rami <103028944+Rami24t@users.noreply.github.com>
2024-04-04 19:42:15 +0200

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -3,23 +3,48 @@
 # PSQL variable for querying the database
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c";
 
+# Function to generate a random number between $1 and $2
+GENERATE_RANDOM_NUMBER() {
+    echo $(($1 + RANDOM % $2))
+}
+
+# Function to check if a username exists in the database
+USERNAME_EXISTS() {
+    local USERNAME="$1"
+    local EXISTS=$($PSQL "SELECT EXISTS (SELECT 1 FROM users WHERE USERNAME='$USERNAME')")
+    if [[ $EXISTS == "t" ]]; then
+        # Username exists
+        return 0
+    else
+        # Username does not exist
+        return 1
+    fi
+}
+
+
 function LOGIN() {
-    USER="$($PSQL "SELECT username, games_played, best_game FROM users WHERE username='$1'")"
-    echo $USER | while IFS='|' read DB_USERNAME DB_GAMES_PLAYED DB_BEST_GAME
-    do
+    # Fetch user from database
+    local USER="$($PSQL "SELECT username, games_played, best_game FROM users WHERE username='$1'")"
+    local DB_USERNAME DB_GAMES_PLAYED DB_BEST_GAME
+    IFS='|' read DB_USERNAME DB_GAMES_PLAYED DB_BEST_GAME <<< $USER
+
+    # Check if username already exists    
         if [[ -z $DB_USERNAME ]]
         then
-            # If that username has not been used before
+            # That username has not been used before
+            # Echo the proper welcome message
             echo "Welcome, $1! It looks like this is your first time here."
-            DB_USERNAME=$1
+            local DB_USERNAME=$1
             return 0
         else
-            # If that username has been used before
-            UPDATE_GAMES_RESULT="$($PSQL "UPDATE users SET games_played=(games_played + 1) WHERE username='$DB_USERNAME'")"
+            # That username has been used before
+            # Update games_played
+            local UPDATE_GAMES_RESULT="$($PSQL "UPDATE users SET games_played=(games_played + 1) WHERE username='$DB_USERNAME'")"
+            # Echo the proper welcome message
             echo "Welcome back, $DB_USERNAME! You have played $(($DB_GAMES_PLAYED+1)) games, and your best game took $DB_BEST_GAME guesses."
             return 1
         fi
-    done
+        return 99
 }
 
 
@@ -56,21 +81,21 @@ function PLAYGAME() {
 function MAIN() {
     
     # randomly generate a number between 1 and 1000 that the users have to guess
-    SECRET_NUMBER=$((1 + $RANDOM % 1000));
+    SECRET_NUMBER=$(GENERATE_RANDOM_NUMBER 1 1000)
     
     # Prompt the user for a username with Enter your username and take a username as input.
     echo -e "Enter your username:";
     read USERNAME_INPUT
     # PS: The database allows usernames that are upto 22 characters long
-    
-    
+
+    IS_NEW_USER=$(USERNAME_EXISTS "$USERNAME")
     echo $(LOGIN $USERNAME_INPUT)
-    USER_EXIST=$?
+
     NUMBER_OF_GUESSES=0
     
     PLAYGAME
     
-    if [[ $USER_EXIST -eq 0 ]]
+    if $IS_NEW_USER
     then
         INSERT_USER_RESULT="$($PSQL "INSERT INTO users(username) VALUES('$USERNAME_INPUT')")"
     fi
